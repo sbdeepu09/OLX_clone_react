a755198e5824cf33b490223e46c424d78c09dd6d
Deepak B <sbdeepu09@gmail.com>
2021-06-12 21:42:39 +0530

    Create post & dynamic homepage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React,{useEffect,useContext} from 'react';
 import {BrowserRouter as Router,Route} from 'react-router-dom'
 import  Signup from './Pages/Signup'
 import Login from './Pages/Login'
+import Create from './Pages/Create'
 import './App.css';
 import {AuthContext, FirebaseContext} from './store/firebaseContext'
 
@@ -31,6 +32,9 @@ function App() {
         </Route>
         <Route path='/login'>
           <Login/>
+        </Route>
+        <Route path='/create'>
+          <Create/>
         </Route>
       </Router>
       
